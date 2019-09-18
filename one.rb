4a79638bbfdcae64ea666ebbfb9a999dce3b9865
Umesh <ukk4732@gmail.com>
2019-09-18 11:04:27 +0530

    Fixed conflicts issue

--- one.rb
+++ one.rb
@@ -4,10 +4,13 @@ class MyEnum
   enum status: {draft: 0, published: 1}
 end
 
+=begin
+  Blog.published!
+  blog = Blog.last
+  blog.published?  
+=end
+
 =begin 
-<<<<<<< HEAD
   Blog.create!(title: "Test blog", body: "Test bodyssss")
-=======
   override commit
->>>>>>> cc2dd6c1a9d636df99adc23cda7bed8d3827a2e5
 =end
