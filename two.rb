5f08c1ea568a159ae12851562d1a4d325f8ec27a
Umesh <ukk4732@gmail.com>
2019-09-20 10:26:31 +0530

    Added master changes - master

--- two.rb
+++ two.rb
@@ -2,6 +2,11 @@
 
 #Print two using downto method
 
-5.downto(-5) { |n| puts "Hello: #{n}" }
+5.downto(-5) { |n| puts "Hello Rails: #{n}" }
 
 # Changes in master branch before rebase
+
+# Master branch changes
+
+# List 1
+# List 2
